eaa78606ac3ba38f964d26045b248b6fc83023e8
Yeldar Issa <yeldar.issa@gmail.com>
2023-05-11 22:42:58 +0600

    connected to external db

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,8 +2,9 @@ import { Pool } from 'pg';
 
 export const pool = new Pool({
   user: 'me',
-  host: 'localhost',
-  database: 'blogs',
-  password: 'password',
+  host: 'dpg-cheh9dl269v75d1t6dp0-a.frankfurt-postgres.render.com',
+  database: 'blogdb_w0n2',
+  password: '8Hr3ZvAQuQdds612cDCVH9I2zXUDlTmQ',
   port: 5432,
+  ssl: true,
 });
