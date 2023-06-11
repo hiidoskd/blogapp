f4e9daed4014249c365ed136c381e815a59eb6fd
Yeldar Issa <yeldar.issa@gmail.com>
2023-06-11 16:07:29 +0600

    moved db config to dotenv

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,16 @@
 import { Pool } from 'pg';
 
+const user = process.env.DB_USER;
+const host = process.env.DB_HOST;
+const database = process.env.DB_DATABASE;
+const password = process.env.DB_PASSWORD;
+const port = +process.env.DB_PORT;
+
 export const pool = new Pool({
-  user: 'me',
-  host: 'dpg-cheh9dl269v75d1t6dp0-a.frankfurt-postgres.render.com',
-  database: 'blogdb_w0n2',
-  password: '8Hr3ZvAQuQdds612cDCVH9I2zXUDlTmQ',
-  port: 5432,
-  ssl: true,
+	user: user,
+	host: host,
+	database: database,
+	password: password,
+	port: port,
+	ssl: true,
 });
